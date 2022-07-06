4bb365d95b5de96d0f93efcf1ab0eda6a2fecf4e
manoj776 <manojadiraju7@gmail.com>
2022-07-06 20:05:19 +0530

    factorial commit

---
+++ src/Factorial.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Factorial {
+
+    static int fact(int n)
+    {
+        if(n==0)
+            return 1;
+
+        return n * fact(n-1);
+    }
+
+    public static void main(String[] args){
+        int n =3;
+        System.out.println(fact(n));
+
+    }
+}
